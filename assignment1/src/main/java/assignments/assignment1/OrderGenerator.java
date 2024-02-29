3c0006ef1dc8df4eb93bbc4b103a0e6776ce9b49
theo <theo.ananda@ui.ac.id>
2024-02-29 08:07:49 +0700

    add generateOrderId, generateBill, and fix validateOrderId, validateLocation

--- assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class OrderGenerator {
     private static final Scanner input = new Scanner(System.in);
     private static int indexOflist = 0;
-    private static String[] listOfOrderId = new String[10];
-    private static String[] listOfOrderDate = new String[10];
-    private static boolean isDone = false;
+    private static boolean isStillLooping = true;
+    private static String[] listOfOrderId = {"null","null","null","null","null","null","null","null","null","null"};
 
     public static boolean validateDate(String orderDate) {
         if (orderDate.length() == 10) return true;
@@ -19,8 +18,8 @@ public class OrderGenerator {
                         return false;
                 }
                 else {
-                    if (Character.isLetter(b))
-                        return false;
+                    if (Character.isDigit(b));
+                    else return false;
                 }
             }
             return true;
@@ -37,76 +36,129 @@ public class OrderGenerator {
     }
 
     public static int validateOrderId (String orderId) {
-        int searchIndex = 0;
-        if (orderId.length()<=16) return 1; // Validasi error karena orderId kurang dari 16 karakter
+        if (orderId.length()<16) return 11; // Validasi error karena orderId kurang dari 16 karakter
         else{
-            for (String elem: listOfOrderId) {
-                if (elem.equals(orderId)) break;
-                else searchIndex++;
+            for (int i = 0; i < 10; i++) {
+                if (listOfOrderId[i].equals(orderId))
+                    return i;
             }
-            if (searchIndex == 9) return 2; // Validasi error karena orderId tidak ada di array
-            else return 3; // Validasi benar       
+           return 22;
         }
     }
 
-    public static int validateLocation (String location){
-        switch(location) {
-            case "P": return 10000;
-            case "U": return 20000;
-            case "T": return 35000;
-            case "S": return 40000;
-            case "B": return 60000;
-            default: return 0; // Validasi error
+    public static String validateLocation (String location){
+        switch(location.toUpperCase()) {
+            case "P": return "Rp 10.000";
+            case "U": return "Rp 20.000";
+            case "T": return "Rp 35.000";
+            case "S": return "Rp 40.000";
+            case "B": return "Rp 60.000";
+            default: return "0"; // Validasi error
         }
     }
+
+    public static int phoneToId (String phoneNumber) {
+        int total = 0;
+        for(int i=0; i<phoneNumber.length(); i++ ) {
+            total += Character.getNumericValue(phoneNumber.charAt(i)); 
+        }
+        return total % 100;
+    }
+
+    public static char checksum (String rawId, int oddEven) { // odd = 1, even =0.
+        int total = 0;
+        int[] listOfCharIn39CharSet = new int[14];
+        for(int i=0; i<14; i++){
+            char c = rawId.charAt(i);
+            if (Character.isDigit(c)) 
+                listOfCharIn39CharSet[i] = Integer.valueOf(rawId.charAt(i))-48;
+            else
+                listOfCharIn39CharSet[i] = Integer.valueOf(rawId.charAt(i))-55;
+        }
+        for (int index = 0; index < 7; index++) {
+            total += listOfCharIn39CharSet[2*index+oddEven];
+        }
+        if (total%36 < 10) return (char)(total%36+48);
+        else return Character.valueOf((char)(total%36+55));
+    }
+
     public static void showMenu(){
         System.out.println(">>=======================================<<");
         System.out.println("|| ___                 ___             _ ||");
-        System.err.println("||| . \\ ___  ___  ___ | __>___  ___  _| |||");
+        System.out.println("||| . \\ ___  ___  ___ | __>___  ___  _| |||");
         System.out.println("||| | |/ ._>| . \\/ ._>| _>/ . \\/ . \\/ . |||");
         System.out.println("|||___/\\___.|  _/\\___.|_| \\___/\\___/\\___|||");
         System.out.println("||          |_|                          ||");
         System.out.println(">>=======================================<<");
         System.out.println();
         System.out.println("Pilih menu:");
-        System.err.println("1. Generate Order ID");
+        System.out.println("1. Generate Order ID");
         System.out.println("2. Generate Bill");
         System.out.println("3. Keluar");
-
+    }
+    public static void orderGenerator () {
+        showMenu();
         System.out.print("Pilihan menu: ");
         int choice = input.nextInt();
         input.nextLine();
         switch (choice) {
             case 1: {
+                System.out.print("Nama Restoran: ");
                 String restoName = input.nextLine();
+                while (restoName.length() < 4){
+                    System.out.println("Masukkan nama resto yang valid! (>=4 karakter)");
+                    System.out.print("Nama Restoran: ");
+                    restoName = input.nextLine();
+                }
+                System.out.print("Tanggal Pemesanan: ");
                 String orderDate = input.nextLine();
-                String phoneNumber = input.nextLine();
-                if (validateDate(orderDate) && validatePhoneNumber(phoneNumber)){
-                    System.out.println(generateOrderID(restoName, orderDate, phoneNumber));
-                    listOfOrderId[indexOflist] = generateOrderID(restoName, orderDate, phoneNumber);
-                    listOfOrderDate[indexOflist] = orderDate;
-                    indexOflist++;
-                }
-                else if (validateDate(orderDate)!=true)
+                while (validateDate(orderDate) == false) {
                     System.out.println("Tanggal pemesanan dalam format DD/MM/YYYY");
-                else
-                    System.out.println("Harap masukkan nomor telepon dalam bentuk bilangan bulat positif!");
+                    System.out.print("Tanggal Pemesanan: ");
+                    orderDate = input.nextLine();
                 }
+                System.out.print("No. Telepon: ");
+                String phoneNumber = input.nextLine();
+                while (validatePhoneNumber(phoneNumber) == false) {
+                    System.out.println("Harap masukkan nomor telepon dalam bentuk bilangan bulat positif!");
+                    System.out.print("No. Telepon: ");
+                    phoneNumber = input.nextLine();
+                }
+                System.out.println("Order ID " + generateOrderID(restoName, orderDate, phoneNumber) + " diterima!");
+                break;
+            }
             case 2: {
                 System.out.print("Order ID: ");
                 String orderId = input.nextLine();
-                if (validateOrderId(orderId) == 1) System.out.println("Order ID minimal 16 karakter!");
-                else if (validateOrderId(orderId) == 2) System.out.println("Silahkan masukkan Order ID yang valid!");
-                else{
-                    System.out.print("Lokasi pengiriman: ");
-                    String location = input.nextLine();
-                    if (validateLocation(location) == 0) System.out.println("Harap masukkan lokasi pengiriman yang ada pada jangkauan!");
-                    else generateBill(orderId, location);
+                while (validateOrderId(orderId) == 11 || validateOrderId(orderId) == 22) {
+                    if (validateOrderId(orderId) == 11){
+                    System.out.println("Order ID minimal 16 karakter!");
+                    System.out.print("Order ID: ");
+                    orderId = input.nextLine();
+                    }
+                    else {
+                        System.out.println("Silahkan masukkan Order ID yang valid!");
+                        System.out.print("Order ID: ");
+                        orderId = input.nextLine();
+                        validateOrderId(orderId);
+                    }
                 }
+                System.out.print("Lokasi pengiriman: ");
+                String location = input.nextLine();
+                location = location.toUpperCase();
+                while (validateLocation(location).equals("0")){ 
+                    System.out.println("Harap masukkan lokasi pengiriman yang ada pada jangkauan!");
+                    System.out.print("Lokasi pengiriman: ");
+                    location = input.nextLine();
+                }
+                System.out.println(generateBill(orderId, location));
+                break;
             }
-            case 3:
+            case 3:{
                 System.out.println("Terima kasih sudah menggunakan DepeFood!");
-                isDone = true;
+                isStillLooping = false;
+            }
+            isStillLooping = false;
         }
     }
 
@@ -117,13 +169,21 @@ public class OrderGenerator {
      * @return String Order ID dengan format sesuai pada dokumen soal
      */
     public static String generateOrderID(String namaRestoran, String tanggalOrder, String noTelepon) {
-        // TODO:Lengkapi method ini sehingga dapat mengenerate Order ID sesuai ketentuan
-        return "TP";
+        String orderId = "";
+        orderId += namaRestoran.substring(0,4).toUpperCase();
+        orderId += tanggalOrder.substring(0,2);
+        orderId += tanggalOrder.substring(3,5);
+        orderId += tanggalOrder.substring(6,10);
+        orderId += phoneToId(noTelepon);
+        orderId += checksum(orderId, 0);
+        orderId += checksum(orderId, 1);
+        listOfOrderId[indexOflist] = orderId;
+        indexOflist++;
+        return orderId;
     }
 
-
     /*
-     * Method ini digunakan untuk membuat bill
+    * Method ini digunakan untuk membuat bill
      * dari order id dan lokasi
      * 
      * @return String Bill dengan format sesuai di bawah:
@@ -134,13 +194,17 @@ public class OrderGenerator {
      *          Biaya Ongkos Kirim: [Total Ongkos Kirim]
      */
     public static String generateBill(String OrderID, String lokasi){
-        // TODO:Lengkapi method ini sehingga dapat mengenerate Bill sesuai ketentuan
-        return "Bill";
+        String orderDate = OrderID.substring(4,6) + "/" + OrderID.substring(6, 8) + "/" + OrderID.substring(8, 12);
+        String bill = "Bill:\n";
+        bill += String.format("Order ID: %s\n", OrderID);
+        bill += String.format("Tanggal Pemesanan: %s\n", orderDate);
+        bill += String.format("Lokasi Pengiriman: %s\n", lokasi.toUpperCase());
+        bill += String.format("Biaya Ongkos Kirim: %s\n", validateLocation(lokasi.toUpperCase()));
+        return bill;
     }
 
     public static void main(String[] args) {
-        // TODO: Implementasikan program sesuai ketentuan yang diberikan
+        while (isStillLooping)
+            orderGenerator();
     }
-
-    
 }
